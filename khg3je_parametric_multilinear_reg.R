aa966b8705d13a341435e6a7c8451598ec2acf61
Kennan Grant <kennangrant@gmail.com>
2017-09-04 16:06:32 -0400

    deleted submission files and created final multilinear regression model

--- khg3je_parametric_multilinear_reg.R
+++ khg3je_parametric_multilinear_reg.R
@@ -1,48 +1,51 @@
+# Assignment: Competition 2 -- Housing Prices
+# Name: Kennan Grant
+# ID: khg3je
+
+
+# Read in Data ------------------------------------------------------------
+
+# load libraries
 library(tidyverse)
 
+# read in train
 train <- read_csv("train.csv")
 train
 
+# read in test
 test <- read_csv("test.csv")
 test
 
 
-# impute for NA -----------------------------------------------------------
+# Explore Data ------------------------------------------------------------
 
-# train %>% 
-#   select(BsmtQual) %>% 
-#   group_by(BsmtQual) %>% 
-#   summarise(n = n())
+# inspect NA's
+sapply(train, function(x) sum(is.na(x)))
 
+# inspect univariate relationships with response
+for (predictor in names(train)) {
+  print(train %>%
+          ggplot(aes_string(x=predictor, y="SalePrice")) +
+          geom_point())
+}
+
+
+# Clean Data --------------------------------------------------------------
+
+# impute character 'NA' in BsmtQual
 train$BsmtQual <- if_else(is.na(train$BsmtQual), "NA", train$BsmtQual)
 test$BsmtQual <- if_else(is.na(test$BsmtQual), "NA", test$BsmtQual)
 
-
+# impute median TotalBsmtSF value where NA
 test$TotalBsmtSF <- if_else(is.na(test$TotalBsmtSF)
                             ,median(test$TotalBsmtSF, na.rm = TRUE)
                             ,as.double(test$TotalBsmtSF))
+
+# impute largest MSZoning class where NA
 test$MSZoning <- if_else(is.na(test$MSZoning), "RL", test$MSZoning)
 
 
-# split training into train and test --------------------------------------
-
-# create ID col
-train <- train %>%
-  mutate(ID = row_number()) 
-
-# set seed for reproducibility
-set.seed(345)
-
-# select 65% of rows for training
-train_sml <- train %>% 
-  sample_frac(size = 0.65) 
-
-# select other 35% of rows for test
-test_sml <- train %>% 
-  filter(!(ID %in% train_sml[["ID"]]))
-
-
-# Explore -----------------------------------------------------------------
+# Create new variables ----------------------------------------------------
 
 
 # split neighborhoods into quintiles based on median saleprice
@@ -114,25 +117,14 @@ test$NeighborhoodQuintile <- test$NeighborhoodQuintile %>%
   factor(levels=c("1", "2", "3", "4", "5"), ordered=TRUE)
 
 
-# add new variables
-train_sml_varsadded <- train_sml %>% 
-  mutate(Fireplaces_YoN = if_else(Fireplaces == 0, 0, 1)
-         ,Log_LA = log(LotArea)
-         ,nearest_decade = round(YearBuilt / 10)
-         ,remod_add_decade = round(YearRemodAdd / 10))
-
-test_sml_varsadded <- test_sml %>% 
-  mutate(Fireplaces_YoN = if_else(Fireplaces == 0, 0, 1)
-         ,Log_LA = log(LotArea)
-         ,nearest_decade = round(YearBuilt / 10)
-         ,remod_add_decade = round(YearRemodAdd / 10))
-
+# add new variables to train
 train_mod <- train %>% 
   mutate(Fireplaces_YoN = if_else(Fireplaces == 0, 0, 1)
          ,Log_LA = log(LotArea)
          ,nearest_decade = round(YearBuilt / 10)
          ,remod_add_decade = round(YearRemodAdd / 10))
 
+# add new variables to test
 test_mod <- test %>% 
   mutate(Fireplaces_YoN = if_else(Fireplaces == 0, 0, 1)
          ,Log_LA = log(LotArea)
@@ -140,92 +132,60 @@ test_mod <- test %>%
          ,remod_add_decade = round(YearRemodAdd / 10))
 
 
+# Cross-Validation ----------------------------------------------------------
 
+library(caret)
 
-# fit final model
-my_model <- lm(SalePrice ~ 
-                 Fireplaces_YoN 
-               + Log_LA 
-               + OverallQual
-               + NeighborhoodQuintile
-               + BsmtQual
-               + MSZoning
-               + TotalBsmtSF
-               + CentralAir
-               + remod_add_decade
-               + ExterQual
-               + FullBath
-               + TotRmsAbvGrd
-               + BedroomAbvGr
-               , data = train_mod )
+formula_for_cv = formula(SalePrice ~ 
+                           Fireplaces_YoN 
+                         + Log_LA 
+                         + OverallQual
+                         + NeighborhoodQuintile
+                         + BsmtQual
+                         + MSZoning
+                         + TotalBsmtSF
+                         + CentralAir
+                         + remod_add_decade
+                         + ExterQual
+                         + FullBath
+                         + TotRmsAbvGrd
+                         + BedroomAbvGr)
 
+# set seed for reproducibility
+set.seed(345)
+
+# perform cross-validation and fit final model
+my_model <- train(formula_for_cv, train_mod, method = "lm"
+               , trControl = trainControl(method = "cv", number = 5
+                                          , verboseIter = TRUE)
+               )
+
+# extract average root mean squared error of folds
+mean_rmse_all_folds <- my_model$results$RMSE
+mean_rmse_all_folds # compare this for each model explored (35264.41 == best yet)
+
+# view model summary
 summary(my_model)
 
-anova(my_model)
+#
+# Notes on model selection:
+#
+# After univariate plots were explored, promising variables (variables that appeared to be associated with the response -- SalePrice -- were put in model. Collinear variables were identified and removed as necessary.  Variables with statistically significant coefficient were retained and then a model iteration was tested.  RMSE was calculated.  The RMSE of different models were compared using k-fold cross-validation (average test RMSE across all folds), and the model with the lowest test RMSE was selected.
+#
 
-# make predictions on TRAIN-TEST
-my_pred <- predict(my_model, newdata=test_sml_varsadded)
-
-# calculate MSE
-my_MSE <- mean((my_pred - test_sml_varsadded$SalePrice)^2)
+# Make Final Predictions for Submission ---------------------------------------
 
 # make predictionson FINAL-TEST
 my_pred2 <- predict(my_model, newdata=test_mod)
-
 
 # create tibble for output
 output <- as_tibble(cbind(test_mod$Id, my_pred2))
 colnames(output) <- c("Id","SalePrice") # rename cols
 
-# remove negative predictions
-output <- output %>%
-  mutate(SalePrice = if_else(SalePrice < 0, 0, SalePrice))
-sum(output$SalePrice < 0) # check count of negative predictions
-
 # write final output to file
-write.csv(output, file = "khg3je_submission6.csv", row.names = FALSE)
+write.csv(output, file = "khg3je_submission7.csv", row.names = FALSE)
 
 
-
-
-# other exploratory stuff -------------------------------------------------
-
-# count NA number
-sapply(train_sml, function(x) sum(is.na(x)))
-
-sapply(my_model$model, function(x) sum(is.na(x)))
-
-sapply(train_mod, function(x) sum(is.na(x)))
-
-
-# drop 1 NA row of TotalBsmtSF, drop 4 MSZoning NA row, 
-
-train %>% 
-  group_by(MasVnrType) %>% 
-  summarise(n = n(), median(SalePrice)) %>% 
-  as.data.frame()
-
-
-for (predictor in names(train)) {
-  
-  
-  print(train %>%
-    ggplot(aes_string(x=predictor, y="SalePrice")) +
-    geom_point())
-  
-}
-
-
-sum(is.na(output$SalePrice))
-
-
-# RoofStyle, MasVnrType
-  
-train %>%
-  mutate(FullBath = as.factor(TotRmsAbvGrd)) %>% 
-        ggplot(aes(x=TotRmsAbvGrd, y=SalePrice)) +
-        geom_point() +
-  geom_boxplot()
 
 
 
